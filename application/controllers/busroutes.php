ea96ecf732d91e0b63ee3f7a880f66aed655f45f
sandali95 <wathmali123@gmail.com>
2017-10-09 22:13:35 +0530

    bus route finder is added.need to implemet ui

---
+++ application/controllers/busroutes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 10/9/2017
+ * Time: 8:56 AM
+ */
---
+++ application/models/bus_model.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 10/9/2017
+ * Time: 12:42 PM
+ */
